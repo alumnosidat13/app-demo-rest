08c754b1ed1e0928f54b7a1ba7662e5553c67623
alumnosidat13 <alumnosidat13@gmail.com>
2025-09-30 22:04:28 -0500

    controller rest terminado

--- src/main/java/pe/edu/idat/app_demo_rest/controller/FundamentosController.java
+++ src/main/java/pe/edu/idat/app_demo_rest/controller/FundamentosController.java
@@ -1,10 +1,9 @@
 package pe.edu.idat.app_demo_rest.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.idat.app_demo_rest.dto.FactorialDto;
+import pe.edu.idat.app_demo_rest.dto.FibonacciDto;
 import pe.edu.idat.app_demo_rest.dto.PrimoDto;
 import pe.edu.idat.app_demo_rest.service.FundamentosService;
 
@@ -16,7 +15,7 @@ public class FundamentosController {
     public FundamentosController(FundamentosService fundamentosService) {
         this.fundamentosService = fundamentosService;
     }
-    //localhost:8055/api/v1/operaciones/primo?numero=5
+    //localhost:8055/api/v1/operacion/primo?numero=5
     @GetMapping("/primo")
     public ResponseEntity<PrimoDto>
             validarNumeroPrimo(
@@ -27,6 +26,35 @@ public class FundamentosController {
                 fundamentosService
                         .validarNumeroPrimo(numero));
         return ResponseEntity.ok(primoDto);
+    }
+
+    //localhost:8055/api/v1/operacion/factorial?numero=5
+    @GetMapping("/factorial")
+    public ResponseEntity<FactorialDto>
+    calcularFactorial(@RequestParam(
+            required = false,
+            defaultValue = "2") Integer numero){
+        FactorialDto factorialDto = new FactorialDto();
+        factorialDto.setNumero(numero);
+        factorialDto.setValorFactorial(
+                fundamentosService
+                        .calcularFactorial(numero));
+        return ResponseEntity.ok(factorialDto);
+    }
+
+    @GetMapping({"/fibonacci", "/fibonacci/{numero}"})
+    public ResponseEntity<FibonacciDto>
+    calcularFibonacci(
+            @PathVariable(required = false)
+            Integer numero){
+        if(numero == null) numero = 1;
+        FibonacciDto fibonacciDto = new FibonacciDto();
+        fibonacciDto.setCantidadNumero(numero);
+        fibonacciDto.setFibonacci(
+                fundamentosService
+                        .calcularFibonacci(numero)
+        );
+        return ResponseEntity.ok(fibonacciDto);
     }
 
 }
